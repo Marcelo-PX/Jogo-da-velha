6278024ac478917be9d8fd7d2ed908060b0cd93b
Marcelo-PX <marcello.mpacheco@gmail.com>
2022-09-26 16:59:37 -0300

    Sequência vencedora v0.3

--- app/src/main/java/br/com/up/projetojogo/MainActivity.kt
+++ app/src/main/java/br/com/up/projetojogo/MainActivity.kt
@@ -1,9 +1,12 @@
 package br.com.up.projetojogo
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -21,9 +24,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     var jogador2 = 1
     var jogadorAtual = jogador1
 
+    lateinit var sequencia : IntArray
+    lateinit var textView : TextView
+
+    var jogadorVencedor = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sequencia = intArrayOf(-1,-1,-1,-1,-1,-1,-1,-1,-1,-1)
+
+        textView = findViewById(R.id.textView2)
 
         b1 = findViewById(R.id.b1)
         b2 = findViewById(R.id.b2)
@@ -46,15 +58,70 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         b9.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-        var btnClicado = findViewById<Button>(v!!.id)
+    override fun onClick(v: View) {
+
+        if(jogadorVencedor)
+            return
+
+        var btnClicado = findViewById<Button>(v.id)
+        var btnTag = Integer.parseInt(btnClicado.tag.toString())
+
+        if(sequencia[btnTag]!= -1)
+            return
+        sequencia[btnTag] = jogadorAtual
 
         if(jogadorAtual == jogador1){
             btnClicado.setText("0")
             jogadorAtual = jogador2
+            textView.setText("Jogador 2")
+            textView.setTextColor(Color.RED)
+            btnClicado.setTextColor(Color.BLACK)
+            btnClicado.backgroundTintList = getColorStateList(R.color.verde)
         }else{
             btnClicado.setText("X")
             jogadorAtual = jogador1
+            textView.setText("Jogador 1")
+            textView.setTextColor(Color.GREEN)
+            btnClicado.setTextColor(Color.BLACK)
+            btnClicado.backgroundTintList = getColorStateList(R.color.vermelho)
         }
+
+        validarVencedor()
+    }
+
+    private fun validarVencedor() {
+
+        var vencedor = arrayOf(
+            intArrayOf(1,2,3),
+            intArrayOf(4,5,6),
+            intArrayOf(7,8,9),
+            intArrayOf(1,4,7),
+            intArrayOf(2,5,8),
+            intArrayOf(3,6,9),
+            intArrayOf(1,5,9),
+            intArrayOf(3,5,7)
+        )
+        for(i in 0 until vencedor.size){
+            var p1 = vencedor[i][0]
+            var p2 = vencedor[i][1]
+            var p3 = vencedor[i][2]
+
+            if(sequencia[p1] == sequencia[p2] && sequencia[p1] == sequencia[p3]){
+
+                if(sequencia[p1]!= -1){
+                    jogadorVencedor = true
+
+                    if(sequencia[p1] == jogador1){
+                        textView.setText("Jogador 1 ganhou!")
+                        textView.setTextColor(Color.GREEN)
+                    }else{
+                        textView.setText("Jogador 2 ganhou!")
+                        textView.setTextColor(Color.RED)
+                    }
+                }
+
+            }
+        }
+
     }
 }
